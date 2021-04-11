82d8e439dafb953cb4a700f46d04ebbaa67abb7d
Ben Shekhtman <benshekhtman@hotmail.com>
2021-04-11 01:01:04 -0400

    more functionality added

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,10 @@
+let imgOne = document.getElementById('one')
+let imgTwo = document.getElementById('two')
+let imgThree = document.getElementById('three')
+let imgFour = document.getElementById('four')
+let imgFive = document.getElementById('five')
+let imgSix = document.getElementById('six')
+let imgSev = document.getElementById('sev')
 //VARIABLES
 let possibleWords = ['cat', 'dog', 'lion', 'zebra', 'tiger', 'bear', 'rhino']
 let secretWord = ''
@@ -9,29 +16,34 @@ let lettersLeft = 0
 let guessTracker = {
     guess: 6
 }
+
 let guesses = document.querySelector('.remain-guess')
 guesses.innerHTML = `Guesses Remaining: ${guessTracker.guess}`;
 
 const resetBtn = document.querySelector('.reset')
 const abcBtn = document.querySelector('.buttons')
-const pastGuess = document.querySelector('.part-guesses')
+
+let pastGuess = document.querySelector('.past-guesses')
+pastGuess.innerHTML = `Past Guesses:`
+
+let img = document.getElementById('.img')
+
 
 //LISTENERS:
 resetBtn.addEventListener('click', function (e){
     start();
 })
 
-function start(){    
+function start(){ 
+    resetBtn.style.display = 'none';   
     secretWord = possibleWords[Math.floor(Math.random() * possibleWords.length)];
     lettersLeft = secretWord.length;    
     for(let n = 0; n < secretWord.length; n++){
         hiddenWord[n] = '_'
     }
-    document.getElementById("secret-word").innerHTML = `Guess the animal: ${hiddenWord.join(' ')}`;    
+    document.getElementById("secret-word").innerHTML = `Guess the movie: ${hiddenWord.join(' ')}`;    
     buttons()
-    
-    console.log(secretWord)
-    console.log(hiddenWord)    
+    images();
 }
 
 function buttons(){
@@ -39,24 +51,65 @@ function buttons(){
         if(e.target.tagName === 'BUTTON' ){
             abcBtn.value = e.target.innerText;
             gamePlay(e.target.innerText.toLowerCase());   
-        }  
+            e.target.setAttribute('disabled','disabled');
+        }
     }) 
 }
 
 function gamePlay(ltr){
+    //IF INCORRECT GUESS
     if(secretWord.indexOf(ltr) === -1){
         pastGuesses.push(ltr)
+        if(guessTracker.guess <= 0)return;
         guessTracker.guess--;
-        console.log(guessTracker.guess)
+        pastGuess.innerHTML = `Past Guesses: ${pastGuesses.join(' ')}`
+        guesses.innerHTML = `Guesses Remaining: ${guessTracker.guess}`;
         render();
 
-    }if (secretWord.indexOf(ltr) > -1){
-        console.log('match')
-    }
-} 
+        //IF CORRECT GUESS
+    }if (secretWord.indexOf(ltr) >= 0){
 
+        //CONVERT UNDERSCORES BACK TO LETTERS
+        lettersLeft--;
+        console.log(hiddenWord)
+        console.log(`match! letterLeft --> ${lettersLeft}`)
+        render();
+    }
+}
 function render(){
     if(guessTracker.guess === 0){
-        console.log('YOU LOSE!')
+        abcBtn.style.display = "none";
+        let winner = document.createElement('h2')
+        winner.innerHTML = 'YOU LOOSE!'
+        let win = document.querySelector('.win-lose')
+        win.appendChild(winner);
+        resetBtn.style.display = 'block';
+        resetBtn.innerHTML = 'Play Again?'; 
+
+    }if(lettersLeft === 0){
+        console.log('YOU WIN!')
+        abcBtn.style.display = "none";
+        let winner = document.createElement('h2')
+        winner.innerHTML = 'YOU WIN!'
+        let win = document.querySelector('.win-lose')
+        win.appendChild(winner);
+        resetBtn.style.display = 'block';
+        resetBtn.innerHTML = 'Play Again?'; 
+
     }
+}
+
+//MAKE RESET FUNCTION!
+function reset(){
+
+}
+
+
+
+function images(){
+    if(guessTracker.guess === 5){
+        imgOne.style.display = 'none';
+        console.log('!')
+    }
+
 }
