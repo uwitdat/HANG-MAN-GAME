da0920d7ec2e77aec2cebacb64ed65a3402b4df6
Ben Shekhtman <benshekhtman@hotmail.com>
2021-04-11 23:49:59 -0400

    game play functionality finished

--- JS/main.js
+++ JS/main.js
@@ -5,22 +5,24 @@ let imgFour = document.getElementById('four')
 let imgFive = document.getElementById('five')
 let imgSix = document.getElementById('six')
 let imgSev = document.getElementById('sev')
+let hintText = document.createElement('h3')
 
+let plyAgn = document.querySelector('.play-again')
 let hint = document.querySelector('.hint')
+
+let winner = document.createElement('h2')
+let win = document.querySelector('.win-lose')
 //VARIABLES
-// let possibleWords = ['cat', 'dog', 'lion', 'zebra', 'tiger', 'bear', 'rhino']
+
 let possibleWords = [
-    {movie: 'cat', hint: '.........'},
-    {movie: 'dog', hint: '1111111111'},
-    {movie: 'lion', hint: '2222222223'},
-    {movie: 'zebra', hint: '3333333333'},
-    {movie: 'tiger', hint: '44444444444'},
-    {movie: 'bear', hint: '5555555555'},
-    {movie: 'rhino', hint:'666666666'}
+    {movie: 'Cat', hint: '.........'},
+    {movie: 'Dog', hint: '1111111111'},
+    {movie: 'Lion', hint: '2222222223'},
+    {movie: 'Zebra', hint: '3333333333'},
+    {movie: 'Tiger', hint: '44444444444'},
+    {movie: 'Bear', hint: '5555555555'},
+    {movie: 'Rhino', hint:'666666666'}
     ]
-
-
-
 
 let secretWord = ''
 let secretHint = ''
@@ -44,36 +46,42 @@ pastGuess.innerHTML = `Past Guesses:`
 
 let img = document.getElementById('.img')
 
+let movieWord = document.getElementById("secret-word")
 
 //LISTENERS:
+
+// START
 resetBtn.addEventListener('click', function (e){
     start();
+})
+
+plyAgn.addEventListener('click', function (e){
+    playAgain();
+ 
+
 })
 
 function start(){ 
     resetBtn.style.display = 'none';   
     let randomizer = possibleWords[Math.floor(Math.random() * possibleWords.length)];
-    secretWord = randomizer.movie
+    secretWord = randomizer.movie.toLowerCase();
     secretHint = randomizer.hint
     lettersLeft = secretWord.length;
+    for(let n = 0; n < secretWord.length; n++) hiddenWord[n] = '_' 
+    movieWord.innerHTML = `Guess the movie: ${hiddenWord.join(' ')}`;   
+    buttons() 
 
-    for(let n = 0; n < secretWord.length; n++){
-        hiddenWord[n] = '_'
-    }
-    document.getElementById("secret-word").innerHTML = `Guess the movie: ${hiddenWord.join(' ')}`;    
-    buttons()
 }
 
 function buttons(){
     abcBtn.addEventListener('click', function(e) {
         if(e.target.tagName === 'BUTTON' ){
             abcBtn.value = e.target.innerText;
+           e.target.setAttribute('disabled', 'disabled')
             gamePlay(e.target.innerText.toLowerCase());   
-            e.target.setAttribute('disabled','disabled');
         }
     }) 
 }
-
 
 function gamePlay(ltr){
     //IF INCORRECT GUESS
@@ -82,40 +90,34 @@ function gamePlay(ltr){
         if(guessTracker.guess <= 0)return;
         guessTracker.guess--;
         renderImage();
-        pastGuess.innerHTML = `Past Guesses: ${pastGuesses.join(' ')}`
+        pastGuess.innerHTML = `Past Guesses: ${pastGuesses.join(' ').toUpperCase()}`
         guesses.innerHTML = `Guesses Remaining: ${guessTracker.guess}`;
-        render();
-
-        //IF CORRECT GUESS
-    }if (secretWord.indexOf(ltr) >= 0){
-
-        //CONVERT UNDERSCORES BACK TO LETTERS
-        lettersLeft--;
-        console.log(hiddenWord)
-        console.log(`match! letterLeft --> ${lettersLeft}`)
-        render();
+        render();   
     }
+        
+        for(let j = 0; j < secretWord.length; j++){
+            if(secretWord[j] === ltr){
+            hiddenWord[j] = ltr
+            movieWord.innerHTML = `Guess the movie: ${hiddenWord.join(' ').toUpperCase()}` 
+            lettersLeft--;
+            render();
+        }   
+    }   
 }
+
 function render(){
     if(guessTracker.guess === 0){
         abcBtn.style.display = "none";
-        let winner = document.createElement('h2')
         winner.innerHTML = `YOU LOOSE! The Correct Movie was: ${secretWord}`
-        let win = document.querySelector('.win-lose')
         win.appendChild(winner);
-        resetBtn.style.display = 'block';
-        resetBtn.innerHTML = 'Play Again?'; 
+        plyAgn.style.display = 'block';
         hint.style.display = "none";
-        
 
     }if(lettersLeft === 0){
         abcBtn.style.display = "none";
-        let winner = document.createElement('h2')
         winner.innerHTML = `YOU WON! The Movie Was: ${secretWord}`
-        let win = document.querySelector('.win-lose')
         win.appendChild(winner);
-        resetBtn.style.display = 'block';
-        resetBtn.innerHTML = 'Play Again?'; 
+        plyAgn.style.display = 'block';
         hint.style.display = "none";
 
     }
@@ -144,13 +146,11 @@ function renderImage(){
     }
 }
 
-
 function renderHint(){
     hint.style.display = "block";
     hint.addEventListener('click',function(e){
     target = e.target;
     target.textContent = secretHint;
-    let hintText = document.createElement('h3')
     hintText.className = 'hint-text'
     hintText.textContent = secretHint;
     document.body.appendChild(hintText)
@@ -158,9 +158,26 @@ function renderHint(){
     })
 }
 
-//MAKE RESET FUNCTION!
-function reset(){
+// // MAKE RESET FUNCTION!
+
+
+function playAgain(){
+    secretWord = ''
+    secretHint = ''
+    hiddenWord = []
+    pastGuesses = []
+    lettersLeft = 0    
+    guessTracker = {
+    guess: 6
+    }
+    guesses.innerHTML = `Guesses Remaining: ${guessTracker.guess}`;
+    pastGuess.innerHTML = `Past Guesses:`
+    winner.innerHTML = ''
+    imgOne.style.display = 'none';
+    imgSev.style.display = 'block';
+    hintText.innerHTML = ''
+    abcBtn.style.display = "block";
+    
+    start();
 
 }
-
-
